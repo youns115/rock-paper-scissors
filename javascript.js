762318023892ab4c81cc403dad93101716e77f31
youns <youns.s.ibrahim115@gmail.com>
2023-05-20 12:07:22 +0300

    add UI

--- javascript.js
+++ javascript.js
@@ -1,66 +1,110 @@
-// rock paper scissors game
+// Rock Paper Scissors game
 
-// random choice for the computer
-function computerChoice(choices,randomChoice){ 
-    choices = [
+let computerWin = 0;
+let playerWin = 0;
+let gameHistory = [];
+
+// Random choice for the computer
+function computerChoice() {
+    const choices = [
         "rock",
         "paper",
         "scissors"
     ];
-    return randomChoice = choices[Math.floor(Math.random()*choices.length)];
-    
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let computerWin = 0;
-let playerWin = 0;
+// Single round game
+function playRound(playerSelection) {
+    const computerSelection = computerChoice();
+    console.log(`The computer chose ${computerSelection}`);
 
-// single round game
-function playGround(playerSelection,computerSelection){ 
-    
-    playerSelection = prompt("rock, paper or scissors?").toLocaleLowerCase()
-    computerSelection = computerChoice()
-    console.log(`the computer chose ${computerSelection}`)
-    
-    // player ties with the computer
-    if (playerSelection == computerSelection){ 
-        console.log("its a tie")
-        
+    updateChoiceDisplay("player-choice", playerSelection);
+    updateChoiceDisplay("computer-choice", computerSelection);
+
+    // Player ties with the computer
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie");
+    }
+    // Player wins
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        console.log("You won this round!");
+        playerWin++;
+    }
+    // Player loses
+    else {
+        console.log("You lost against the computer");
+        computerWin++;
     }
 
-    // player wins
-    else if ((playerSelection=="rock" && computerSelection =="scissors")||(playerSelection=="scissors" && computerSelection=="paper")||(playerSelection=="paper" && computerSelection=="rock")){ 
-         console.log("you won this round!")
-         playerWin++
-    }
+    // Add the round history to the game history
+    gameHistory.push({ player: playerSelection, computer: computerSelection });
 
-    // player loses
-    else{ 
-        console.log("you lost against a fkin robot retard")
-        computerWin++
-    }
-    
-    
-    
+    updateResultDisplay();
+    updateHistoryDisplay();
+    checkWinner();
 }
-// 5 round game
-function game(){ 
-    for (let i =0; i<5;i++){ 
-        playGround()
-        
-    }
-    if(playerWin>computerWin){ 
 
-        alert("YOU WON THE GAME!")
-    }
-    else if(playerWin==computerWin){ 
-        alert("ITS A TIE")
-    }
-    else{ 
-        alert("YOU LOST")
-        
-    }
-    console.log(`player round wins: ${ playerWin}`)
-    console.log(`computer round wins: ${computerWin}`)
-    
+// UI
+function updateResultDisplay() {
+    const resultDisplay = document.getElementById("result");
+    resultDisplay.textContent = `Player Wins: ${playerWin}, Computer Wins: ${computerWin}`;
 }
-game()
+
+function updateChoiceDisplay(elementId, choice) {
+    const choiceDisplay = document.getElementById(elementId);
+    choiceDisplay.textContent = choice;
+}
+
+function updateHistoryDisplay() {
+    const historyList = document.getElementById("history-list");
+    historyList.innerHTML = "";
+
+    gameHistory.forEach((round) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `Player: ${round.player} - Computer: ${round.computer}`;
+        historyList.appendChild(listItem);
+    });
+}
+
+
+function checkWinner() {
+    if (playerWin >= 5) {
+        alert("YOU WON THE GAME!");
+        resetGame();
+    } else if (computerWin >= 5) {
+        alert("YOU LOST");
+        resetGame();
+    }
+}
+
+function resetGame() {
+    playerWin = 0;
+    computerWin = 0;
+    gameHistory = [];
+    updateResultDisplay();
+    updateChoiceDisplay("player-choice", "");
+    updateChoiceDisplay("computer-choice", "");
+    updateHistoryDisplay();
+}
+
+function onClickButton(playerSelection) {
+    playRound(playerSelection);
+}
+
+// Event listeners for the buttons
+const rockButton = document.getElementById("rock-button");
+rockButton.addEventListener("click", () => onClickButton("rock"));
+
+const paperButton = document.getElementById("paper-button");
+paperButton.addEventListener("click", () => onClickButton("paper"));
+
+const scissorsButton = document.getElementById("scissors-button");
+scissorsButton.addEventListener("click", () => onClickButton("scissors"));
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
